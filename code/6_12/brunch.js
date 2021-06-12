87af0a5d9c07b369913ed1e34553868a9fd84872
Seongjunsim <48894731+Seongjunsim@users.noreply.github.com>
2021-06-12 18:16:00 +0900

    gppd

--- code/6_12/brunch.js
+++ code/6_12/brunch.js
@@ -1,14 +1,23 @@
 const puppeteer = require('puppeteer');
-
+//pptr . dev
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+      headless : false,
+  });
   const page = await browser.newPage();
   await page.setViewport({
       width : 1440,
-      height : 900
+      height : 900,
   });
-  await page.goto('https://brunch.co.kr/search?q=IT&type=article');
-  await page.screenshot({ path: 'brunch.png' });
+  await page.goto('https://brunch.co.kr/search');
+  await page.click("input.txt_search");
+  await page.keyboard.type('it');
+  await page.keyboard.press("Enter");
+  // os 가 다를 경우 access안됨
+  
+  await page.waitForNavigation();
 
-  await browser.close();
+  await page.evaluate(() => {
+      console.log("dho dkseho tlqkf ");
+  });
 })();
