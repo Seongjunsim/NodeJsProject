b22bf3777828f9c9669295ff99740b9e97ea40e2
Seongjunsim <48894731+Seongjunsim@users.noreply.github.com>
2021-06-17 13:02:54 +0900

    good

--- code/5_29/index.js
+++ code/5_29/index.js
@@ -1,10 +1,37 @@
 const axios = require("axios");
-const cheerio = require("cheerio");
+const fs = require("fs");
+let article = {};
+const crawler = (pageNumber) => {
+    return axios.get(
+        "https://api.brunch.co.kr/v1/search/article?"+
+        `q=Hello%20World&page=${pageNumber}&pageSize=20&highlighter=y&escape=y&sortBy=accu`
+        )
+        .then((response) => {
+        const htmlString = response.data;
+        //const $ = cheerio.load(htmlString);
+        //const h1 = $('h1').text(); // h1에 할당된 텍스트 획득
+        console.log("current page number: ", pageNumber);
+        article[pageNumber] = htmlString.data.list.map(item => {
+            return {
+                title: item.title,
+                contentSummary: item.contentSummary,
+                contentId: item.contentId
+            }
+        });
+        const nextNumber = pageNumber + 1;
+        if(nextNumber < 10){
+            crawler(nextNumber);
+            return;
+        }
+        fs.writeFile("brunch_article.json", JSON.stringify(article), (err, data)=>{
+            if(err){
+                console.error(err);
+                return;
+            }
+            console.log("success file write");
+        });
+    });
+};
 
-axios.get("https://www.tistory.com/category/getMoreCategoryPost.json?category=life&lastPublished=0&first=true").then((response) => {
-    const htmlString = response.data;
-    //const $ = cheerio.load(htmlString);
-    //const h1 = $('h1').text(); // h1에 할당된 텍스트 획득
-    
-    console.dir(htmlString.data.list);
-});
+const data = crawler(1);
+
