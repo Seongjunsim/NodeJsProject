dd209491746430b2be9f8ca0b60d5356ac6a5e33
Seongjunsim <48894731+Seongjunsim@users.noreply.github.com>
2021-06-19 16:42:56 +0900

    good

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
+import CustomList from './components/CustomList';
 import MainHeader from './components/MainHeader';
 
+
+//state, props 
+// props -> 부모 요소에서 관리 -> 수동적 : 부모에서 적어준 데이터에 의존 
+// state -> 컴포넌트 그 자체에서 관리
 function App() {
+  const array1 = ["apple", "banana", "orange"];
+  // return (
+  //   <div className="App">
+  //     <ul>
+  //       {array1.map((value, index) => {
+  //         return (
+  //           <CustomList text={`${value} ${index}`}></CustomList>
+  //         );
+  //       })}
+  //     </ul>
+  //   </div>
+  // );
+
   return (
     <div className="App">
-      <MainHeader></MainHeader>
+      <h1>rkwmdi</h1>
+      <MainHeader text="hi"></MainHeader>
     </div>
   );
 }
--- project1/src/components/MainHeader.js
+++ project1/src/components/MainHeader.js
@@ -1,12 +1,33 @@
-import React , {useState} from 'react';
+import React, { useState } from 'react';
+import './MainHeader.scss';
 
-function MainHeader() {
+
+//  SASS -> css 변수 할당, 깔끔한 코드
+const style = {
+    color : "red"
+};
+
+
+function MainHeader(props) {
     // 상태를 처리하는 함수
     // [상태 데이터, 상태를 위한 setter함수]
-    const [text, setClickState] = React.useState("false");
+    //const [text, setClickState] = React.useState("false");
+    //return <h1 className="main_header">{props.text}</h1>;
+    const [color, setColor] = useState("red");
     
+    // 인라인 style 방식은 추천하지 않는다 -> 상위라서 유지보수가 어렵당
+    //return(
+    //    <h1
+    //    style = {{color: color}}
+    //    onClick = {()=>{setColor(color==="red" ? "green":"red");}}
+    //    >{props.text}</h1>
+    //);
+
     return(
-        <h1 onClick={() => {setClickState("Bye world!")}}>{text}</h1>
+        <h1
+        className = {color}
+        onClick = {()=>{setColor(color==="red" ? "green":"red");}}
+        >{props.text}</h1>
     );
     
 }
